6062e8ce257bc6675cad75cf6c8850667f8a248d
Matt Ray <matthewhray@gmail.com>
2016-05-28 11:15:09 -0500

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'matt@chef.io'
 license          'Apache 2.0'
 description      'Installs zoneminder security camera software'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 supports         'ubuntu'
 supports         'debian'
 source_url       'https://github.com/mattray/zoneminder-cookbook' if respond_to?(:source_url)
